9f5ea1b38a31d75336c6bc8a37c5a570eccf785b
Daniel Gwon <daniel.gwon@ll.mit.edu>
2025-01-23 07:24:57 -0500

    Remove initial message

--- chit_chat/client.py
+++ chit_chat/client.py
@@ -3,7 +3,7 @@ import asyncio
 import chit_chat.constants as con
 
 
-async def tcp_echo_client(message, host, port):
+async def tcp_echo_client(host, port):
     reader, writer = await asyncio.open_connection(host=host, port=port)
     
     while True:
@@ -21,19 +21,13 @@ async def tcp_echo_client(message, host, port):
     await writer.wait_closed()
 
 
-if __name__ == '__main__':
-    num_args = len(sys.argv)
-    if num_args == 1:
-        print('Please supply a message')
-        print('Usage: python client.py MESSAGE HOST PORT')
-    message = sys.argv[1]
-    
-    if num_args < 4:
+if __name__ == '__main__':    
+    if len(sys.argv) < 3:
         host = con.HOST
         port = con.PORT
     else:
-        host = sys.argv[2]
-        port = int(sys.argv[3])
+        host = sys.argv[1]
+        port = int(sys.argv[2])
     
-    asyncio.run(tcp_echo_client(message, host, port))
+    asyncio.run(tcp_echo_client(host, port))
     
